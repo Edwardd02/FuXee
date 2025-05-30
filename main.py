805436376f94d097cf4f9df3894f2495e3cf0c96
Edwardd02 <renxuanyao@foxmail.com>
2025-05-30 17:57:49 +0800

    Icon added, Version 0.0.1

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import flet as ft
 from flet_gui import main
+import os
+import re
+from tkinter import messagebox
+from i18n.languages import LanguageManager
+import flet_gui
+import file_operations
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, view=ft.FLET_APP)
